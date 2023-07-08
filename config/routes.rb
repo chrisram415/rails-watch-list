9bd66ed2c3b165e7ed01a78e30589bf094af3780
Christopher Ramirez Rodriguez <chrisram415@gmail.com>
2023-07-08 16:24:55 +0900

    implemented seeds, routes, and copntrollers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: [:index, :show, :new, :create]
+
+  resources :bookmarks, only: [:new, :create, :destroy]
+
 end
+
+# You don’t need all 7 CRUD routes for your models (and ZERO routes for your movies):
+# lists
+# index, show, new, create
+# bookmarks
+# new, create, destroy
