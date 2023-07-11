af5d9dd87a1fc8547b66d20ea2f43ac23a1ffecc
Christopher Ramirez Rodriguez <chrisram415@gmail.com>
2023-07-11 21:11:36 +0900

    implemented list controller and view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,16 +1,29 @@
 class ListsController < ApplicationController
-
+  # my logic
   def index
     @lists = List.all
   end
 
   def show
+    # raise
+    @list = List.find(params[:id])
   end
 
   def new
+    # the form
+    @list = List.new
   end
 
   def create
+    # stores info
+    # raise
+    @list = List.create(list_params)
+    redirect_to list_path(@list)
   end
 
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/new'
+  get 'lists/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :lists, only: [:index, :show, :new, :create]
+  root "lists#index"
+  # resources :lists, only: [:index, :show, :new, :create]
 
-  resources :bookmarks, only: [:new, :create, :destroy]
+  # resources :bookmarks, only: [:new, :create, :destroy]
+
+  resources :lists, only: [:show, :new, :create] do
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
 
 end
 
